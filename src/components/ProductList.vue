7d54882eebd22831fc278e9441d14e140653f3c8
Angelina0100 <angelinae259@gmail.com>
2020-08-23 19:42:06 +0300

    3.5 - ProductList component added, images added

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,40 +1,72 @@
 <template>
   <ul class="catalog__list">
-    <li class="catalog__item" v-for="(product, index) in products" v-bind:key="index">
-      <a class="catalog__pic" href="#">
-        <img v-bind:src="product.image" v-bind:alt="product.title">
+    <li
+      v-for="(product, index) in products"
+      :key="index"
+      class="catalog__item"
+    >
+      <a
+        class="catalog__pic"
+        href="#"
+      >
+        <img
+          :src="product.image"
+          :alt="product.title"
+        >
       </a>
 
       <h3 class="catalog__title">
         <a href="#">
-          {{product.title}}
+          {{ product.title }}
         </a>
       </h3>
 
       <span class="catalog__price">
-              {{product.price}}
-            </span>
+        {{ product.price }}
+      </span>
 
       <ul class="colors colors--black">
         <li class="colors__item">
           <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-            <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
+            <input
+              class="colors__radio sr-only"
+              type="radio"
+              name="color-1"
+              value="#73B6EA"
+              checked=""
+            >
+            <span
+              class="colors__value"
+              style="background-color: #73B6EA;"
+            />
           </label>
         </li>
         <li class="colors__item">
           <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-            <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
+            <input
+              class="colors__radio sr-only"
+              type="radio"
+              name="color-1"
+              value="#8BE000"
+            >
+            <span
+              class="colors__value"
+              style="background-color: #8BE000;"
+            />
           </label>
         </li>
         <li class="colors__item">
           <label class="colors__label">
-            <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-            <span class="colors__value" style="background-color: #222;">
-                  </span>
+            <input
+              class="colors__radio sr-only"
+              type="radio"
+              name="color-1"
+              value="#222"
+            >
+            <span
+              class="colors__value"
+              style="background-color: #222;"
+            />
           </label>
         </li>
       </ul>
@@ -43,7 +75,7 @@
 </template>
 
 <script>
-  export default {
-    props: ['products']
-  }
+export default {
+  props: ['products'],
+};
 </script>
