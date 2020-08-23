3fee33f8a2e077387724a1a44623079d802f5e2b
Angelina0100 <angelinae259@gmail.com>
2020-08-23 21:22:12 +0300

    3.5 - ProductItem component added, images added

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,81 +1,17 @@
 <template>
   <ul class="catalog__list">
-    <li
+    <ProductItem
       v-for="(product, index) in products"
       :key="index"
-      class="catalog__item"
-    >
-      <a
-        class="catalog__pic"
-        href="#"
-      >
-        <img
-          :src="product.image"
-          :alt="product.title"
-        >
-      </a>
-
-      <h3 class="catalog__title">
-        <a href="#">
-          {{ product.title }}
-        </a>
-      </h3>
-
-      <span class="catalog__price">
-        {{ product.price }}
-      </span>
-
-      <ul class="colors colors--black">
-        <li class="colors__item">
-          <label class="colors__label">
-            <input
-              class="colors__radio sr-only"
-              type="radio"
-              name="color-1"
-              value="#73B6EA"
-              checked=""
-            >
-            <span
-              class="colors__value"
-              style="background-color: #73B6EA;"
-            />
-          </label>
-        </li>
-        <li class="colors__item">
-          <label class="colors__label">
-            <input
-              class="colors__radio sr-only"
-              type="radio"
-              name="color-1"
-              value="#8BE000"
-            >
-            <span
-              class="colors__value"
-              style="background-color: #8BE000;"
-            />
-          </label>
-        </li>
-        <li class="colors__item">
-          <label class="colors__label">
-            <input
-              class="colors__radio sr-only"
-              type="radio"
-              name="color-1"
-              value="#222"
-            >
-            <span
-              class="colors__value"
-              style="background-color: #222;"
-            />
-          </label>
-        </li>
-      </ul>
-    </li>
+    />
   </ul>
 </template>
 
 <script>
+import ProductItem from './ProductItem.vue';
+
 export default {
+  components: { ProductItem },
   props: ['products'],
 };
 </script>
